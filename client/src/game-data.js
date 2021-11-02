524bcda275721f6701e38a51efd4ccc1167ccc36
Lê Trung Hiếu ( Henry Le ) <lthieu1599@gmail.com>
2021-11-02 13:57:35 +0700

    Update description of the game-data

--- client/src/game-data.js
+++ client/src/game-data.js
@@ -5,7 +5,7 @@ const game_data = [
     {
         "name": "Ori and the blind forest",
         "price": 250000,
-        "description": "The forest of Nibel is dying. After a powerful storm sets a series of devastating events in motion, Ori must journey to find courage and confront a dark nemesis to save the forest of Nibel. “Ori and the Blind Forest” tells the tale of a young orphan destined for heroics, through a visually stunning Action-Platformer crafted by Moon Studios. Featuring hand-painted artwork, meticulously animated character performance, a fully orchestrated score and dozens of new features in the Definitive Edition, “Ori and the Blind Forest” explores a deeply emotional story about love and sacrifice, and the hope that exists in us all.",
+        "description": "The forest of Nibel is dying. After a powerful storm sets a series of devastating events in motion, Ori must journey to find courage and confront a dark nemesis to save the forest of Nibel.",
         "publisher": "Moon Studio",
         "icon": "../images/Ori_and_the_blind_forest_icon.png"
     },
@@ -14,7 +14,7 @@ const game_data = [
     {
         "name": "Ori and the will of the wisps",
         "price": 180000,
-        "description": "The little spirit Ori is no stranger to peril, but when a fateful flight puts the owlet Ku in harm’s way, it will take more than bravery to bring a family back together, heal a broken land, and discover Ori’s true destiny. From the creators of the acclaimed action-platformer Ori and the Blind Forest comes the highly anticipated sequel. Embark on an all-new adventure in a vast world filled with new friends and foes that come to life in stunning, hand-painted artwork. Set to a fully orchestrated original score, Ori and the Will of the Wisps continues the Moon Studios tradition of tightly crafted platforming action and deeply emotional storytelling.",
+        "description": "The little spirit Ori is no stranger to peril, but when a fateful flight puts the owlet Ku in harm’s way, it will take more than bravery to bring a family back together, heal a broken land, and discover Ori’s true destiny.",
         "publisher": "Moon Studio",
         "icon": "../images/Ori_and_the_will_of_the_wisps_icon.png"
     },
@@ -46,7 +46,7 @@ const game_data = [
     {
         "name": "Muck",
         "price": 25000,
-        "description": "Trapped in the muck on an island, alone or with your friends, try to survive as long as possible by using the resources you find around the island. Collect resources, craft tools, weapons, & armor, find items & build your base during day. But once night falls, mysterious enemies appear from the shadows. Using the resources and items you've crafted during the day, you must try and make it through the night.",
+        "description": "Trapped in the muck on an island, alone or with your friends, try to survive as long as possible by using the resources you find around the island. Collect resources, craft tools, weapons, & armor, find items & build your base during day.",
         "publisher": "Dani Dev Stuff",
         "icon": "../images/Muck_icon.png"
     },
@@ -54,7 +54,7 @@ const game_data = [
     {
         "name": "Crab Game",
         "price": 20000,
-        "description": "Crab Game is a First-Person Multiplayer game where you fight for the ultimate cash price in different minigames based on children's games, until only one player is left to claim it all. Definitely not based on any online streaming pop culture korean tv shows, as that would get me in legal trouble, so we're certainly not doing that.",
+        "description": "Crab Game is a First-Person Multiplayer game where you fight for the ultimate cash price in different minigames based on children's games, until only one player is left to claim it all.",
         "publisher": "Dani Dev Stuff",
         "icon": "../images/Crab_game_icon.png"
     },
@@ -70,7 +70,7 @@ const game_data = [
     {
         "name": "Fall Guys",
         "price": 290000,
-        "description": "Fall Guys: Ultimate Knockout flings hordes of contestants together online in a wild dash through round after round of escalating chaos until one victor remains! Battle bizarre obstacles, shove through unruly competitors, and overcome the unbending laws of physics as you stumble towards greatness. Leave your dignity at the door and prepare for hilarious failure in your quest to claim the crown!",
+        "description": "Fall Guys: Ultimate Knockout flings hordes of contestants together online in a wild dash through round after round of escalating chaos until one victor remains! Battle bizarre obstacles, shove through unruly competitors",
         "publisher": "Mediatonic",
         "icon": "../images/Fall_guy_icon.png"
     },
@@ -78,7 +78,7 @@ const game_data = [
     {
         "name": "Among us",
         "price": 52500,
-        "description": "Play with 4-15 player online or via local WiFi as you attempt to prepare your spaceship for departure, but beware as one or more random players among the Crew are Impostors bent on killing everyone! Originally created as a party game, we recommend playing with friends at a LAN party or online using voice chat. Enjoy cross-platform play between Android, iOS, Nintendo Switch, and PC. Coming soon to Xbox and PlayStation, which will also feature crossplay.",
+        "description": "Play with 4-15 player online or via local WiFi as you attempt to prepare your spaceship for departure, but beware as one or more random players among the Crew are Impostors bent on killing everyone!",
         "publisher": "Innersloth",
         "icon": "../images/Among_us_icon.png"
     }
