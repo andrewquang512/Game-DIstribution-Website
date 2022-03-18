bfd6b8e1a2b18cb454f82f1be5aca167cb1f11f2
andrewquang512 <andrew.viquang@gmail.com>
2022-03-18 23:52:39 +0700

    change for deploy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import { errorHandler, notFound } from './middlewares/errorHandler.js';
 import multer from 'multer';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 var storage = multer.diskStorage({
