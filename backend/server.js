06f460d9d183070f9ddd75b17c7b0cf45f3c0dfe
andrewquang512 <andrew.viquang@gmail.com>
2022-03-18 23:09:42 +0700

    change for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,48 @@
-import express from 'express'
-import path from 'path'
-import gamesRoute from './routes/gameRoutes.js'
-import userRoute from './routes/userRoutes.js'
-import orderRoute from './routes/orderRoutes.js'
-import connectDB from './config/db.js'
-import { errorHandler, notFound } from './middlewares/errorHandler.js'
-import multer from 'multer'
+import express from 'express';
+import path from 'path';
+import gamesRoute from './routes/gameRoutes.js';
+import userRoute from './routes/userRoutes.js';
+import orderRoute from './routes/orderRoutes.js';
+import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middlewares/errorHandler.js';
+import multer from 'multer';
 
-const app = express()
-const port = 5000
-const __dirname = path.resolve()
+const app = express();
+const port = 5000;
+const __dirname = path.resolve();
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
-    }
-  })
-   
-  var upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
 
-connectDB()
-console.log(__dirname)
-app.use(express.json())
-app.use('/static', express.static(path.join(__dirname, 'public')))
+var upload = multer({ storage: storage });
 
-app.use('/api/games', gamesRoute)
-app.use('/api/users', userRoute)
-app.use('/api/order', orderRoute)
+connectDB();
+console.log(__dirname);
+app.use(express.json());
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.use(notFound)
-app.use(errorHandler)
+app.use('/api/games', gamesRoute);
+app.use('/api/users', userRoute);
+app.use('/api/order', orderRoute);
 
-app.get('/', (req, res) => res.send('Hello all!'))
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is running at http://localhost:${port}`))
+app.get('/', (req, res) => res.send('Hello all!'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`)
+);
