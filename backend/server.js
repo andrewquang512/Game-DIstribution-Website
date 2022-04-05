122560093b8d2dad71ce3c5b92c58cd8d209ff76
andrewquang512 <andrew.viquang@gmail.com>
2022-04-05 23:37:40 +0700

    fix for deploy

--- backend/server.js
+++ backend/server.js
@@ -29,22 +29,12 @@ app.use('/api/games', gamesRoute);
 app.use('/api/users', userRoute);
 app.use('/api/order', orderRoute);
 
-app.use(notFound);
-app.use(errorHandler);
-
 // app.get('/', (req, res) => res.send('Hello all!'));
 
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  // ? get('*') get anything from roots here and load to serve index.html
-  app.get('*', (req, res) => {
-    // ? __dirname get current directory
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
-
-app.listen(port, () =>
-  console.log(`Server is running at http://localhost:${port}`)
-);
+app.listen(port, () => console.log(`Server started on port ${port}`));
